0753062815b60c01e2d83237b78a0a721d02ddc8
svasenkov <aanher@gmail.com>
2021-02-19 21:35:56 +0300

    Added closeWebdriver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
 
@@ -41,5 +42,6 @@ public class TestBase {
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
+        closeWebDriver();
     }
 }
